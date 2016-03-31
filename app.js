716f76f074fcfb481a4a0443516ef6f280ca91b5
Nick MacIntyre <nickmac@Nick-MacBook-Air.local>
2016-03-31 12:45:57 -0600

    about to install dev

--- app.js
+++ app.js
@@ -4,11 +4,36 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+var passport = require('passport')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+passport.use(new LinkedInStrategy({
+  clientID: LINKEDIN_CLIENT_ID,
+  clientSecret:	LINKEDIN_CLIENT_SECRET  ,
+  callbackURL: "http://localhost:3000/auth/linkedin/callback",
+  scope: ['r_emailaddress', 'r_basicprofile'],
+}, function(accessToken, refreshToken, profile, done) {
+  // asynchronous verification, for effect...
+  process.nextTick(function () {
+    // To keep the example simple, the user's LinkedIn profile is returned to
+    // represent the logged-in user. In a typical application, you would want
+    // to associate the LinkedIn account with a user record in your database,
+    // and return that user instead.
+    return done(null, profile);
+  });
+}));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user)
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,10 +46,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
 
+app.get('/auth/linkedin',
+passport.authenticate('linkedin', { state: 'SOME STATE'  }),
+function(req, res){
+  // The request will be redirected to LinkedIn for authentication, so this
+  // function will not be called.
+});
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
